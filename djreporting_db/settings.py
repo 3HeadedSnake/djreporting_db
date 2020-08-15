163537cf57e9a4abf1636e114ab59ee4af6c8065
troyegilbert <gilberttroye@gmail.com>
2020-08-15 21:31:54 +0300

    updated settings.py

--- djreporting_db/settings.py
+++ djreporting_db/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0q7)d6+*u-^10no%md=-m4*sp3l7n%74otbgwxk^*v-vk^tw7o'
+SECRET_KEY = " "
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
